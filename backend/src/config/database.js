51670478f8726586c06d2115eed09863cc43373e
Livia <liviamtavares05@gmail.com>
2025-12-05 20:14:17 -0300

    outras coisas

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -6,13 +6,18 @@ class Database {
   }
 
   init() {
-    this.db = new Sequelize({
-      database: 'agenda',
-      host: 'localhost',
-      username: 'root',
-      password: '',
-      dialect: 'mysql'
-    })
+    this.db = new Sequelize(
+      process.env.DB_NAME || 'agenda',
+      process.env.DB_USER || 'root',
+      process.env.DB_PASS || '',
+      {
+        host: process.env.DB_HOST || 'localhost',
+        dialect: 'mysql',
+        port: process.env.DB_PORT || 3306,
+        // No Docker interno do Render, geralmente não precisa de SSL obrigatório, 
+        // mas é bom deixar preparado caso precise.
+      }
+    )
   }
 }
 
